d9caab10ec6294d79077646faceea22505fb9939
olof <aksberg.olof@outlook.com>
2022-07-11 19:23:19 +0200

    create LayoutSettings comp

--- client/src/components/employees/employeesContainer/EmployeesContainer.tsx
+++ client/src/components/employees/employeesContainer/EmployeesContainer.tsx
@@ -9,6 +9,7 @@ import {
 import "./employeesContainer.style.scss";
 import { EmployeeListCard, EmployeeMainCard } from "../employeeCards";
 import { useEffect, useState } from "react";
+import { LayoutSettings } from "./components";
 
 export const EmployeesContainer = () => {
  const dispatch = useDispatch();
@@ -27,20 +28,7 @@ export const EmployeesContainer = () => {
 
  return (
   <>
-   <div className="layouts">
-    <i
-     className="fa-solid fa-list"
-     onClick={() => {
-      if (layout !== 1) dispatch(setLayout(1));
-     }}
-    ></i>
-    <i
-     className="fa-solid fa-qrcode"
-     onClick={() => {
-      if (layout !== 0) dispatch(setLayout(0));
-     }}
-    ></i>
-   </div>
+   <LayoutSettings />
    <section
     className={`employees-container ${layout === 0 ? "cards" : "list"}`}
    >
