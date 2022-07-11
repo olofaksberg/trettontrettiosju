2abba206f527178fcb94d153d9477ec054884af4
olof <aksberg.olof@outlook.com>
2022-07-11 17:34:18 +0200

    create html structure for ToolsArea comp

--- client/src/components/layouts/toolsArea/ToolsArea.tsx
+++ client/src/components/layouts/toolsArea/ToolsArea.tsx
@@ -1,7 +1,68 @@
 /** @format */
 
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { offices } from "../../../constants/employees";
+import { employeesData } from "../../../store/employees/employeesSlice";
+import { If } from "../../helpers";
+import { Button, InputText, InputCheckbox } from "../general";
+
 import "./toolsArea.style.scss";
 
 export const ToolsArea = () => {
- return <section className="tools-container"></section>;
+ const { employees } = useSelector(employeesData);
+
+ const [filter, setFilter] = useState({
+  name: "",
+  office: [],
+ });
+
+ return (
+  <section className="tools-container">
+   <h1>Search Employees</h1>
+   <section className="input-container flex JC-SB">
+    <div className="flex FG-4">
+     <div className="m-top-1">
+      <InputText
+       name="search"
+       label="Search employee"
+       value={filter.name}
+       action={() => console.log("swag")}
+      />
+
+      <If condition={filter.name !== ""}>
+       <div className="searchRes">
+        {employees
+         .filter((d: any) => d.name.includes(filter.name))
+         .map((d: any) => {
+          return <div onClick={() => console.log("swag")}>{d.name}</div>;
+         })}
+       </div>
+      </If>
+     </div>
+
+     <div className="flex FD-C JC-SA">
+      {offices.map((d) => {
+       return (
+        <InputCheckbox name={d} label={d} action={() => console.log("swag")} />
+       );
+      })}
+     </div>
+    </div>
+
+    <div className="flex FD-C JC-C AI-C FG-2">
+     <Button
+      text="Reset"
+      iconClassName="fa-solid fa-minus"
+      action={() => console.log("swag")}
+     />
+     <Button
+      text="Search"
+      iconClassName="fa-solid fa-magnifying-glass"
+      action={() => console.log("swag")}
+     />
+    </div>
+   </section>
+  </section>
+ );
 };
