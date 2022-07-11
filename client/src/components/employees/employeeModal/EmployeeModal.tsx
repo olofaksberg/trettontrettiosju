b6a26cb0622768ea058231e24a4ba0dc98ebec71
olof <aksberg.olof@outlook.com>
2022-07-11 18:00:16 +0200

    create EmployeeModal comp and style it

---
+++ client/src/components/employees/employeeModal/EmployeeModal.tsx
@@ -0,0 +1,39 @@
+/** @format */
+
+import { useDispatch, useSelector } from "react-redux";
+import {
+ employeesActions,
+ employeesData,
+} from "../../../store/employees/employeesSlice";
+
+import { Image } from "../../layouts";
+
+import "./employeeModal.style.scss";
+
+export const EmployeeModal = () => {
+ const dispatch = useDispatch();
+ const { chosenEmployee } = useSelector(employeesData);
+ const { setChosenEmployee } = useSelector(employeesActions);
+
+ return (
+  <section className="employee-modal">
+   <div className="container flex JC-SB">
+    <div className="modal-left">
+     <h1 className="emp-name">{chosenEmployee.name}</h1>
+
+     <div>{chosenEmployee.mainText}</div>
+    </div>
+    <div className="modal-right">
+     <Image source={chosenEmployee.imageWallOfLeetUrl} />
+    </div>
+
+    <div
+     className="close-modal"
+     onClick={() => dispatch(setChosenEmployee(null))}
+    >
+     <i className="fa-solid fa-xmark"></i>
+    </div>
+   </div>
+  </section>
+ );
+};
