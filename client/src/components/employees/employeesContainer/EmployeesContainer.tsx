89960701967bbebb550c27275634e44b17f00741
olof <aksberg.olof@outlook.com>
2022-07-11 19:03:58 +0200

    move setLayout to EmployeesContainer

--- client/src/components/employees/employeesContainer/EmployeesContainer.tsx
+++ client/src/components/employees/employeesContainer/EmployeesContainer.tsx
@@ -1,14 +1,19 @@
 /** @format */
 
-import { useSelector } from "react-redux";
-import { employeesData } from "../../../store/employees/employeesSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+ employeesActions,
+ employeesData,
+} from "../../../store/employees/employeesSlice";
 
 import "./employeesContainer.style.scss";
 import { EmployeeListCard, EmployeeMainCard } from "../employeeCards";
 import { useEffect, useState } from "react";
 
 export const EmployeesContainer = () => {
+ const dispatch = useDispatch();
  const { employees, filteredEmployees, layout } = useSelector(employeesData);
+ const { setLayout } = useSelector(employeesActions);
 
  const [showData, setShowData] = useState([]);
 
@@ -21,14 +26,32 @@ export const EmployeesContainer = () => {
  }, [filteredEmployees]);
 
  return (
-  <section className={`employees-container ${layout === 0 ? "cards" : "list"}`}>
-   {showData.map((d: any, i: number) => {
-    return layout === 1 ? (
-     <EmployeeListCard employee={d} />
-    ) : (
-     <EmployeeMainCard employee={d} />
-    );
-   })}
-  </section>
+  <>
+   <div className="layouts">
+    <i
+     className="fa-solid fa-list"
+     onClick={() => {
+      if (layout !== 1) dispatch(setLayout(1));
+     }}
+    ></i>
+    <i
+     className="fa-solid fa-qrcode"
+     onClick={() => {
+      if (layout !== 0) dispatch(setLayout(0));
+     }}
+    ></i>
+   </div>
+   <section
+    className={`employees-container ${layout === 0 ? "cards" : "list"}`}
+   >
+    {showData.map((d: any, i: number) => {
+     return layout === 1 ? (
+      <EmployeeListCard employee={d} />
+     ) : (
+      <EmployeeMainCard employee={d} />
+     );
+    })}
+   </section>
+  </>
  );
 };
--- client/src/components/layouts/toolsArea/ToolsArea.tsx
+++ client/src/components/layouts/toolsArea/ToolsArea.tsx
@@ -14,8 +14,8 @@ import "./toolsArea.style.scss";
 
 export const ToolsArea = () => {
  const dispatch = useDispatch();
- const { employees, layout } = useSelector(employeesData);
- const { setLayout, setFilteredEmployees } = useSelector(employeesActions);
+ const { employees } = useSelector(employeesData);
+ const { setFilteredEmployees } = useSelector(employeesActions);
 
  const [filter, setFilter] = useState({
   name: "",
@@ -25,97 +25,83 @@ export const ToolsArea = () => {
  return (
   <section className="tools-container">
    <h1>Search Employees</h1>
-   <section className="input-container flex JC-SB">
-    <div className="flex FG-4">
-     <div className="m-top-1">
-      <InputText
-       name="search"
-       label="Search employee"
-       value={filter.name}
-       action={(e) =>
-        setFilter((prev: any) => {
-         return {
-          ...prev,
-          name: e.target.value,
-         };
-        })
-       }
-      />
+   <section className="input-container flex JC-C FG-10">
+    {/* <div className="flex FG-4"> */}
+    <div className="m-top-1">
+     <InputText
+      name="search"
+      label="Name"
+      value={filter.name}
+      action={(e) =>
+       setFilter((prev: any) => {
+        return {
+         ...prev,
+         name: e.target.value,
+        };
+       })
+      }
+     />
 
-      <If condition={filter.name !== ""}>
-       <div className="searchRes">
-        {employees
-         .filter((d: any) => d.name.includes(filter.name))
-         .map((d: any) => {
-          return (
-           <div
-            onClick={() => {
-             setFilter((prev: any) => {
-              return {
-               ...prev,
-               name: d.name,
-              };
-             });
-            }}
-           >
-            {d.name}
-           </div>
-          );
-         })}
-       </div>
-      </If>
-     </div>
-
-     <div className="flex FD-C JC-SA">
-      {offices.map((d) => {
-       return (
-        <InputCheckbox
-         name={d}
-         label={d}
-         action={(e) =>
-          setFilter((prev: any) => {
-           return {
-            ...prev,
-            office: prev.office.includes(e.target.name)
-             ? prev.office.filter((d: any) => d !== e.target.name)
-             : [...prev.office, e.target.name],
-           };
-          })
-         }
-        />
-       );
-      })}
-     </div>
+     <If condition={filter.name !== ""}>
+      <div className="searchRes">
+       {employees
+        .filter((d: any) => d.name.includes(filter.name))
+        .map((d: any) => {
+         return (
+          <div
+           onClick={() => {
+            setFilter((prev: any) => {
+             return {
+              ...prev,
+              name: d.name,
+             };
+            });
+           }}
+          >
+           {d.name}
+          </div>
+         );
+        })}
+      </div>
+     </If>
     </div>
 
-    <div className="flex FD-C JC-C AI-C FG-2">
-     <Button
-      text="Reset"
-      iconClassName="fa-solid fa-minus"
-      action={() => dispatch(setFilteredEmployees(false))}
-      customStyle={{ background: "rgb(255, 98, 98)" }}
-     />
-     <Button
-      text="Search"
-      iconClassName="fa-solid fa-magnifying-glass"
-      action={() => dispatch(setFilteredEmployees(filter))}
-      customStyle={{ background: "#0c0c91" }}
-     />
+    <div className="flex FD-C JC-SA">
+     <h4>Offices</h4>
+     {offices.map((d) => {
+      return (
+       <InputCheckbox
+        name={d}
+        label={d}
+        action={(e) =>
+         setFilter((prev: any) => {
+          return {
+           ...prev,
+           office: prev.office.includes(e.target.name)
+            ? prev.office.filter((d: any) => d !== e.target.name)
+            : [...prev.office, e.target.name],
+          };
+         })
+        }
+       />
+      );
+     })}
     </div>
+    {/* </div> */}
    </section>
-   <div className="layouts">
-    <i
-     className="fa-solid fa-list"
-     onClick={() => {
-      if (layout !== 1) dispatch(setLayout(1));
-     }}
-    ></i>
-    <i
-     className="fa-solid fa-qrcode"
-     onClick={() => {
-      if (layout !== 0) dispatch(setLayout(0));
-     }}
-    ></i>
+   <div className="flex JC-C FG-2">
+    <Button
+     text="Reset"
+     iconClassName="fa-solid fa-minus"
+     action={() => dispatch(setFilteredEmployees(false))}
+     customStyle={{ background: "rgb(255, 98, 98)" }}
+    />
+    <Button
+     text="Search"
+     iconClassName="fa-solid fa-magnifying-glass"
+     action={() => dispatch(setFilteredEmployees(filter))}
+     customStyle={{ background: "#0c0c91" }}
+    />
    </div>
   </section>
  );
